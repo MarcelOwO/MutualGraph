5b1ca121d6b2c07cce780e3b03b0217395f6f9d6
Marcel <Marcel-sigl@gmx.de>
2025-11-27 20:09:23 +0100

    adding mutual api and updaing main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::{thread::sleep, time::Duration};
+
+use vrchatapi::models::LimitedUserFriend;
+
 mod api;
 mod mutual;
 
@@ -5,11 +9,48 @@ mod mutual;
 async fn main() {
     let config = api::Init(String::from("MutualGraph")).await;
 
-    let player_id = "";
+    let total_friends = 300;
 
-    let mut mutuals = mutual::get_mutuals(&config, player_id).await.unwrap();
+    let mut offset = 0;
+    let n = 100;
 
-    for mutual in mutuals.iter_mut() {
-        println!("{}", mutual);
+    let mut friends: Vec<LimitedUserFriend> = vec![];
+
+    while offset <= total_friends {
+        match api::apis::friends_api::get_friends(&config, Some(offset), Some(n), Some(true)).await
+        {
+            Ok(result) => {
+                for friend in result {
+                    friends.push(friend);
+                }
+            }
+            Err(e) => {
+                println!("Failed to get Friends: {}", e);
+            }
+        };
+
+        offset += friends.len() as i32;
+        //Delay because of vrchat api rules
+        sleep(Duration::from_secs(60));
+    }
+
+    let mut mutuals: Vec<LimitedUserFriend> = vec![];
+
+    for friend in friends {
+        let player_uuid = friend.id;
+
+        match mutual::get_mutuals(&config, player_uuid).await {
+            Ok(result) => {
+                for mutual in result {
+                    mutuals.push(mutual);
+                }
+            }
+            Err(e) => {
+                println!("Error getting mutual: {}", e);
+            }
+        };
+
+        //Delay because of vrchat api rules
+        sleep(Duration::from_secs(60));
     }
 }
--- src/mutual.rs
+++ src/mutual.rs
@@ -1,11 +1,7 @@
 use std::io::Error;
 
-use reqwest;
 use serde::{Deserialize, Serialize};
-use vrchatapi::{
-    apis::{ResponseContent, configuration},
-    models,
-};
+use vrchatapi::{apis::configuration, models};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum GetMutualsError {
@@ -15,8 +11,8 @@ pub enum GetMutualsError {
 
 pub async fn get_mutuals(
     configuration: &configuration::Configuration,
-    user_id: &str,
-) -> Result<(), Error<GetMutualsError>> {
+    user_id: String,
+) -> Result<Vec<models::LimitedUserFriend>, Error<GetMutualsError>> {
     let local_config = configuration;
     let client = local_config.client;
 
@@ -25,12 +21,40 @@ pub async fn get_mutuals(
         local_config.base_path, user_id,
     );
 
-    let mut local_req_builder = client.request(reqwest::Method::GET, uri.as_str());
+    let mut local_var_req_builder = client.request(reqwest::Method::GET, uri.as_str());
 
     if let Some(ref local_user_agent) = local_config.user_agent {
-        local_req_builder =
-            local_req_builder.header(reqwest::header::USER_AGENT, local_user_agent.clone());
+        local_var_req_builder =
+            local_var_req_builder.header(reqwest::header::USER_AGENT, local_user_agent.clone());
     };
 
-    let local_req = local_req_builder.build()?;
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<GetFriendsError> =
+            serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent {
+            status: local_var_status,
+            content: local_var_content,
+            entity: local_var_entity,
+        };
+        Err(Error::ResponseError(local_var_error))
+    }
+
+    let local_req = match local_req_builder.build() {
+        Ok(result) => {}
+        Err(e) => {
+            println!("Error in building request: {}", e);
+        }
+    };
+
+    let mutual = vec!["a", "b"];
+
+    Ok(mutual)
 }
